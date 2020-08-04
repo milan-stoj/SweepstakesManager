6831181c8944dd8ea034430ca528ddc74c64778c
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-08-04 12:52:47 -0500

    factory design pattern for manager created

---
+++ Sweepstakes/ManagerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class ManagerFactory : ICreateManagers
+    {
+        public ISweepstakesManager SelectManager(string type)
+        {
+            if (type == "Queue")
+            {
+                return new SweepstakesQueueManager();
+            }
+            else
+            {
+                return new SweepstakesQueueManager();
+            }
+        }
+    }
+}
